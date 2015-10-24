ca4e87f62f364559fac4a844a755d76be41e9ba5
Aimee Reiss <akmreiss@yahoo.com>
2015-10-24 14:53:19 -0700

    Grab username and place at top of WelcomeActivity

---
+++ app/src/main/java/com/epicodus/lifestock/WelcomeActivity.java
@@ -0,0 +1,24 @@
+package com.epicodus.lifestock;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        String username = getIntent().getStringExtra("Username");
+
+        TextView tv = (TextView)findViewById(R.id.userName);
+        tv.setText(username);
+
+
+    }
+
+
+}
