67b3c12a1309fd82a5b2ccc913cfcc4b108e0cbe
Aimee Reiss <akmreiss@yahoo.com>
2015-11-04 23:45:31 -0800

    Add adapter

---
+++ app/src/main/java/com/epicodus/lifestock/adapter/ListAdapter.java
@@ -0,0 +1,7 @@
+package com.epicodus.lifestock.adapter;
+
+/**
+ * Created by oem on 11/4/15.
+ */
+public class ListAdapter {
+}
--- app/src/main/java/com/epicodus/lifestock/ui/NewSitingActivity.java
+++ app/src/main/java/com/epicodus/lifestock/ui/NewSitingActivity.java
@@ -1,9 +1,11 @@
 package com.epicodus.lifestock.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -25,14 +27,16 @@ public class NewSitingActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_siting);
         ButterKnife.bind(this);
+
+        mSubmitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(NewSitingActivity.this, SitingsListActivity.class);
+
+            }
+        });
     }
 
 
 }
 
-//
-//mSpecies = (TextView) findViewById(R.id.speciesText);
-//        mLocation = (TextView) findViewById(R.id.locationText);
-//        mImage = (ImageView) findViewById(R.id.animalImage);
-//        mNotes = (TextView) findViewById(R.id.notesText);
-//        mButton = (Button) findViewById(R.id.nextButton);
